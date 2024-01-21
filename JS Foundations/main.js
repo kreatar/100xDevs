5527c3cd6199063504da130edcdd0478f6fd3de4
Mohammed Yaser <yasermohammeddev@gmail.com>
2024-01-22 01:19:30 +0530

    JS file created

---
+++ JS Foundations/main.js
@@ -0,0 +1,15 @@
+const allUsers = [{
+    firstName: "Yaser",
+    gender: "male"
+}, {
+    firstName: "Yaser",
+    gender: "male",
+    metadata: {
+        age: 21,
+        address: ""
+    }
+}, {
+    firstName: "Kaleem",
+    gender: "male"
+}]
+
